9969584ecbb0286201b9d0f618b1e34994c73e1c
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-06-03 16:47:45 +0700

    ADD

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,6 +56,8 @@
                                  </a>
                                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a href="{{url('home')}}"> Profile</a>
+                                    <a href="{{url('mahasiswa')}}"> College Data</a>
+                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                             document.getElementById('logout-form').submit();">
                                        {{ __('Logout') }}
@@ -63,7 +65,7 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                        @csrf
                                     </form>
-                                    <a href="{{url('mahasiswa')}}"> College Data</a>
+                                    
 
                                  </div>
                               </li>
